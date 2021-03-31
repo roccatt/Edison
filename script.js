1bf1f863b9045d404f6e0cda01f9c179320bfa3e
roccatt <80890275+roccatt@users.noreply.github.com>
2021-03-31 09:21:20 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function on(){
+  document.getElementById("imgstart").src = "https://www.activefamilymag.com/wp-content/uploads/2012/05/tumblr_kojjphDRDa1qzsoymo1_4001.png"
+  
+}
+function off(){
+  document.getElementById("imgstart").src = "https://www.industrytap.com/wp-content/uploads/2016/02/incandescent-e1456179151174.jpg"
+  
+}
